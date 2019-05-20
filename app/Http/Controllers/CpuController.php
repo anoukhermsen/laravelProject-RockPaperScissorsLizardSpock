35f5f14c7098380c028d1a7325669bda88cbfa32
Anouk Hermsen <anouk@enrise.com>
2019-05-20 09:13:25 +0200

    start

---
+++ app/Http/Controllers/CpuController.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class CpuController
+{
+    const ROCK = 1;
+    const PAPER = 2;
+    const SCISSORS = 3;
+    const LIZARD = 4;
+    const SPOCK = 5;
+
+    protected $cpu;
+    protected $username2;
+
+    public function __construct(string $cpu = 'CPU')
+    {
+        $this->cpu = $cpu;
+    }
+
+    public function showCpu()
+    {
+        $cpu = $this->cpu;
+
+        return $cpu;
+    }
+
+    public function choices()
+    {
+
+        $choices = array(
+            self::ROCK => array('scissors', 'lizard'),
+            self::PAPER => array('rock', 'spock'),
+            self::SCISSORS => array('paper', 'lizard'),
+            self::LIZARD => array('spock', 'paper'),
+            self::SPOCK => array('scissors', 'rock')
+        );
+        return $choices;
+    }
+
+
+}
---
+++ resources/views/create.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Rock Paper Scissors Lizard Spock</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .content {
+                font-size: 20px;
+                margin-left: 10px;
+            }
+
+            .title {
+                font-size: 50px;
+                text-align: center;
+            }
+
+            select{
+                font-size: 15px;
+            }
+
+            input[type="submit" i]{
+                font-size: 15px;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="content">
+            <div class="title">
+                Rock Paper Scissors Lizard Spock
+            </div>
+            <div class="container">
+                <div class="input-group">
+                    <form action="#" method="get">
+                        Name:
+                        <input type="text" name="name">
+                        <br>
+                        Choice:
+                        <select name="choice">
+                            <option value="rock">Rock</option>
+                            <option value="paper">Paper</option>
+                            <option value="scissors">Scissors</option>
+                            <option value="lizard">Lizard</option>
+                            <option value="spock">Spock</option>
+                        </select>
+                        <br><br>
+                        <input type="submit" name="submit" value="Submit">
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
